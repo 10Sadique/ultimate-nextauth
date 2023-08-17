70779fc09108c6085de4775a601b58fe57ffc3cb
10Sadique <jafarsjahan@gmail.com>
2023-08-17 18:37:31 +0600

    register form added

--- src/app/(site)/register/page.jsx
+++ src/app/(site)/register/page.jsx
@@ -1,3 +1,37 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { RegisterForm } from "@/components/register-form";
+import Link from "next/link";
+
 export default function RegisterPage() {
-  return <div>RegisterPage</div>;
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Card className="w-96">
+        <CardHeader>
+          <CardTitle>Register</CardTitle>
+          <CardDescription>Create a new account.</CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <RegisterForm />
+        </CardContent>
+
+        <CardFooter>
+          <p>
+            Already have an account?{" "}
+            <Link className="underline" href={"/login"}>
+              Login
+            </Link>
+            .
+          </p>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
