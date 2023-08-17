a6f1d5d1320433981ec787f521b57318c7b7bb16
10Sadique <jafarsjahan@gmail.com>
2023-08-17 18:37:24 +0600

    login form added

--- src/app/(site)/login/page.jsx
+++ src/app/(site)/login/page.jsx
@@ -1,3 +1,37 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { LoginForm } from "@/components/login-form";
+import Link from "next/link";
+
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Card className="w-96">
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>Login to your account.</CardDescription>
+        </CardHeader>
+
+        <CardContent>
+          <LoginForm />
+        </CardContent>
+
+        <CardFooter>
+          <p>
+            Don&apos;t have an account?{" "}
+            <Link className="underline" href={"/register"}>
+              Register
+            </Link>
+            .
+          </p>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
