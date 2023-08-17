8f1c7bdb7fcbd4b10e00161bdcef2f377ea0689d
10Sadique <jafarsjahan@gmail.com>
2023-08-17 18:37:11 +0600

    updated

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,28 @@
-import Image from "next/image";
+import { getServerSession } from "next-auth";
 
-export default function Home() {
+import { User } from "@/components/user";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { SignOutButton } from "@/components/sign-out-button";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
+
+export default async function Home() {
+  const session = await getServerSession(authOptions);
   return (
-    <section>
+    <section className="p-4">
       <h1>Home</h1>
+      <h1>Server Side Rendered</h1>
+      <pre>{JSON.stringify(session, null, 2)}</pre>
+
+      <User />
+
+      <div className="flex items-center pt-4 space-x-4">
+        <SignOutButton />
+
+        <Link href={"/login"} className={buttonVariants()}>
+          Login
+        </Link>
+      </div>
     </section>
   );
 }
