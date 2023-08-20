25b1ab42bb7e463e606d28b3b07aad34c8c505c0
10Sadique <jafarsjahan@gmail.com>
2023-08-20 12:29:11 +0600

    design changed

--- src/app/(site)/login/page.jsx
+++ src/app/(site)/login/page.jsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import { ChevronLeft } from "lucide-react";
+
 import {
   Card,
   CardContent,
@@ -7,31 +10,44 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { LoginForm } from "@/components/login-form";
-import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
 
 export default function LoginPage() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <Card className="w-96">
-        <CardHeader>
-          <CardTitle>Login</CardTitle>
-          <CardDescription>Login to your account.</CardDescription>
-        </CardHeader>
+    <div className="relative grid h-screen grid-cols-1 lg:grid-cols-2">
+      <Link
+        className={buttonVariants({
+          className: "absolute top-4 left-4",
+          variant: "secondary",
+        })}
+        href={"/"}
+      >
+        <ChevronLeft className="w-4 h-4 mr-2" />
+        Back
+      </Link>
+      <div className="hidden w-full h-full lg:block bg-zinc-600"></div>
+      <div className="flex items-center justify-center h-full">
+        <Card className="w-96">
+          <CardHeader>
+            <CardTitle>Login</CardTitle>
+            <CardDescription>Login to your account.</CardDescription>
+          </CardHeader>
 
-        <CardContent>
-          <LoginForm />
-        </CardContent>
+          <CardContent>
+            <LoginForm />
+          </CardContent>
 
-        <CardFooter>
-          <p>
-            Don&apos;t have an account?{" "}
-            <Link className="underline" href={"/register"}>
-              Register
-            </Link>
-            .
-          </p>
-        </CardFooter>
-      </Card>
+          <CardFooter>
+            <p>
+              Don&apos;t have an account?{" "}
+              <Link className="underline" href={"/register"}>
+                Register
+              </Link>
+              .
+            </p>
+          </CardFooter>
+        </Card>
+      </div>
     </div>
   );
 }
--- src/app/(site)/register/page.jsx
+++ src/app/(site)/register/page.jsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import { ChevronLeft } from "lucide-react";
+
 import {
   Card,
   CardContent,
@@ -6,32 +9,46 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { buttonVariants } from "@/components/ui/button";
 import { RegisterForm } from "@/components/register-form";
-import Link from "next/link";
 
 export default function RegisterPage() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <Card className="w-96">
-        <CardHeader>
-          <CardTitle>Register</CardTitle>
-          <CardDescription>Create a new account.</CardDescription>
-        </CardHeader>
+    <div className="relative grid h-screen grid-cols-1 lg:grid-cols-2">
+      <Link
+        className={buttonVariants({
+          className: "absolute top-4 left-4",
+          variant: "secondary",
+        })}
+        href={"/"}
+      >
+        <ChevronLeft className="w-4 h-4 mr-2" />
+        Back
+      </Link>
+      <div className="hidden w-full h-full lg:block bg-zinc-600"></div>
 
-        <CardContent>
-          <RegisterForm />
-        </CardContent>
+      <div className="flex items-center justify-center h-full">
+        <Card className="w-96">
+          <CardHeader>
+            <CardTitle>Register</CardTitle>
+            <CardDescription>Create a new account.</CardDescription>
+          </CardHeader>
 
-        <CardFooter>
-          <p>
-            Already have an account?{" "}
-            <Link className="underline" href={"/login"}>
-              Login
-            </Link>
-            .
-          </p>
-        </CardFooter>
-      </Card>
+          <CardContent>
+            <RegisterForm />
+          </CardContent>
+
+          <CardFooter>
+            <p>
+              Already have an account?{" "}
+              <Link className="underline" href={"/login"}>
+                Login
+              </Link>
+              .
+            </p>
+          </CardFooter>
+        </Card>
+      </div>
     </div>
   );
 }
