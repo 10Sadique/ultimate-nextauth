52080f3a71e7109fc0eb7a0e17cf1a51f4dff3b5
10Sadique <jafarsjahan@gmail.com>
2023-08-20 12:28:57 +0600

    style changed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,26 +2,18 @@ import { getServerSession } from "next-auth";
 
 import { User } from "@/components/user";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import { SignOutButton } from "@/components/sign-out-button";
-import Link from "next/link";
-import { buttonVariants } from "@/components/ui/button";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
   return (
     <section className="container p-4">
-      <h1>Home</h1>
-      <h1>Server Side Rendered</h1>
-      <pre>{JSON.stringify(session, null, 2)}</pre>
+      <div className="grid grid-cols-2 gap-8 mt-4">
+        <div className="p-4 border rounded-md bg-zinc-800">
+          <h1>Server Side Rendered</h1>
+          <pre>{JSON.stringify(session, null, 2)}</pre>
+        </div>
 
-      <User />
-
-      <div className="flex items-center pt-4 space-x-4">
-        <SignOutButton />
-
-        <Link href={"/login"} className={buttonVariants()}>
-          Login
-        </Link>
+        <User />
       </div>
     </section>
   );
